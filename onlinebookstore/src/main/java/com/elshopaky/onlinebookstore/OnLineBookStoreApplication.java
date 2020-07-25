87514f9af4d7f42380609b084121033453a44200
Tamer <Tamer@192.168.1.5>
2020-07-25 06:17:43 +0300

    createc spring boot application and configure the application properties

---
+++ onlinebookstore/src/main/java/com/elshopaky/onlinebookstore/OnLineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.elshopaky.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnLineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnLineBookStoreApplication.class, args);
+	}
+
+}
---
+++ onlinebookstore/src/test/java/com/elshopaky/onlinebookstore/OnLineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elshopaky.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnLineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
